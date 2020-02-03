85f030e97fd38b3082a6987a2e4353a412eb2601
gtsayan <48355689+gtsayan@users.noreply.github.com>
2020-02-03 08:42:34 +0300

    2

---
+++ src/blocks/modules/card/__wineglass-slider/card__wineglass-slider.js
@@ -0,0 +1,13 @@
+modules.define('card__wineglass-slider', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declElem('card', 'wineglass-slider', {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
